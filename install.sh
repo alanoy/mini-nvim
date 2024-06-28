b32c2509a274bc3dba12b376a548f85f9fb1129d
Alan <alanoy11@gmail.com>
2024-06-28 14:08:21 +0800

    improve install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 wget https://github.com/neovim/neovim/releases/download/v0.10.0/nvim-linux64.tar.gz
 tar xzf nvim-linux64.tar.gz --strip-components 1 -C /usr/local/
-mkdir -p ~/.config/nvim
 
-# TODO: create init.lua
+mkdir -p ~/.config/nvim
+wget https://raw.githubusercontent.com/alanoy/mini-nvim/main/init.lua ~/.config/nvim
 
 sed -i '/alias l='"'"'ls -CF'"'"'/a alias vi='"'"'nvim'"'"'' ~/.bashrc
 . ~/.bashrc
